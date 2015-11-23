eaf2352057f45586056d82854fb28858918a92de
Ivan Kozik <ivan@ludios.org>
2015-11-23 07:25:07 +0000

    Add program to CRC32C stdin

---
+++ stdin_crc32c.py
@@ -0,0 +1,20 @@
+import sys
+from _crc32c.lib import sse4_crc32c
+from cffi import FFI
+ffi = FFI()
+
+def crc32c_for_file(f, block_size=64*1024):
+	assert block_size >= 1, "block_size must be >= 1, was %r" % (block_size,)
+	mem = ffi.new('char[65536]')
+	c = 0
+	arr = ffi.buffer(mem)
+	while True:
+		num_bytes_read = f.readinto(arr)
+		if not num_bytes_read:
+			break
+		c = sse4_crc32c(c, mem, num_bytes_read)
+	return c
+
+
+if __name__ == '__main__':
+	print crc32c_for_file(sys.stdin)
