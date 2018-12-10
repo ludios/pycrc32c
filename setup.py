fb69a50631387d9deaa88b4e08a2e2c7ff973070
Ivan Kozik <ivan@ludios.org>
2018-12-10 01:05:57 +0000

    Update for Python 3 and NixOS build environment

--- setup.py
+++ setup.py
@@ -7,17 +7,19 @@ from distutils.extension import Extension
 
 import build_crc32c
 
-os.environ['CFLAGS'] = " ".join(["-O3", "-march=native", "-Wall"])
+os.environ['CFLAGS'] = " ".join(["-O3", "-march=native", "-msse4.2", "-Wall"])
 
 setup(
 	name='pycrc32c',
-	version='1.0.0',
+	version='2.0.0',
 	description="crc32c for Python; uses the Intel CRC32 instruction and thus requires SSE4.2",
 	url="https://github.com/ludios/pycrc32c",
 	author="Ivan Kozik",
 	author_email="ivan@ludios.org",
 	classifiers=[
-		'Development Status :: 4 - Beta',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 3',
+		'Development Status :: 5 - Stable',
 		'Operating System :: POSIX :: Linux',
 		'Intended Audience :: Developers',
 		'License :: OSI Approved :: MIT License',
