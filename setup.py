516d55a43d5342ea392549ba5bdd57feb3f9061b
Ivan Kozik <ivan@ludios.org>
2018-12-12 01:13:23 +0000

    Make setup.py work when cffi is not installed yet
    https://cffi.readthedocs.io/en/latest/cdef.html

--- setup.py
+++ setup.py
@@ -2,29 +2,27 @@
 
 import os
 import sys
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
 
-import build_crc32c
-
-os.environ['CFLAGS'] = " ".join(["-O3", "-msse4.2", "-Wall"])
+os.environ["CFLAGS"] = " ".join(["-O3", "-msse4.2", "-Wall"])
 
 setup(
-	name='pycrc32c',
-	version='2.0.0',
+	name="pycrc32c",
+	version="2.0.0",
 	description="crc32c for Python; uses the Intel CRC32 instruction and thus requires SSE4.2",
 	url="https://github.com/ludios/pycrc32c",
 	author="Ivan Kozik",
 	author_email="ivan@ludios.org",
 	classifiers=[
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 3',
-		'Development Status :: 5 - Stable',
-		'Operating System :: POSIX :: Linux',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 3",
+		"Development Status :: 5 - Stable",
+		"Operating System :: POSIX :: Linux",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
 	],
-	py_modules=['crc32c'],
-	ext_modules=[build_crc32c.ffibuilder.distutils_extension()],
+	py_modules=["crc32c"],
+	setup_requires=["cffi"],
+	cffi_modules=["build_crc32c.py:ffibuilder"],
 	install_requires=["cffi"]
 )
